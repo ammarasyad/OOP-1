d407fab581dbe6a31d447dff2a1e5fc2877f1425
zidane-itb <16521420@mahasiswa.itb.ac.id>
2023-03-14 22:50:38 +0700

    update inventory generics

--- src/inventory/headers/inventory.hpp
+++ src/inventory/headers/inventory.hpp
@@ -11,8 +11,8 @@ class Inventory {
         int deckSize;
     public:
         Inventory();
-        virtual void addToDeck(PlayerCard&) = 0;
-        virtual std::vector<PlayerCard>& operator+(PlayerCard&) = 0;
+        virtual void addToDeck(T&) = 0;
+        virtual std::vector<T>& operator+(T&) = 0;
 };
 
 #endif //IF2210_PEMROGRAMAN_BERORIENTASI_OBJEK_INVENTORY_HPP
