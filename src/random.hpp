b10c0cfecc2f43dcf2827d76297e24349b2ff4df
ammarasyad <ammarasyad@gmail.com>
2023-03-15 21:42:05 +0700

    fix: Add upperBound parameter instead of hard-coded bound for RNG

--- src/random.hpp
+++ src/random.hpp
@@ -2,10 +2,10 @@
 #define IF2210_PEMROGRAMAN_BERORIENTASI_OBJEK_RANDOM_HPP
 
 // x86 RDRAND Intrinsic, pls pake ini aja ty (buat manual g++ users ini dicompile pake flag -mrdrnd)
-inline unsigned int getRandom() {
+inline unsigned int getRandom(int upperBound) {
     unsigned int result;
     __builtin_ia32_rdrand32_step(&result);
-    return result % 13 + 1; // Hasil bakal 1-13
+    return result % upperBound;
 }
 
 #endif
