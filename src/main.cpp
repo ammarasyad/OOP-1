37e3c863d3e36deca215eb57f80960f5d19bc4d3
zidane-itb <16521420@mahasiswa.itb.ac.id>
2023-03-16 14:33:12 +0700

    initial commit for main

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,22 @@
+#include "gameio.hpp"
+#include "gamestate.hpp"
+#include "gamestatefactory.hpp"
+
 int main() {
-    return 0;
+    // config
+    int playerCount = 7;
+
+    // end of config
+
+    std::vector<Player> playerList;
+
+    for (int i  = 0; i < playerCount; ++i) {
+        playerList.emplace_back();
+    }
+
+    GameIO gameIo;
+    GameState s(playerList);
+    s.startGame();
+    gameIo.startGame(s);
+
 }
